c45685705dff20456b88b5b44c39c28a3ba83750
NamPE286 <nambuihung654@gmail.com>
2025-03-04 10:09:49 +0700

    change window size

--- src/common.h
+++ src/common.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr auto WINDOW_WIDTH = 640;
-constexpr auto WINDOW_HEIGHT = 480;
+constexpr auto WINDOW_WIDTH = 1280;
+constexpr auto WINDOW_HEIGHT = 720;
 constexpr auto FIXED_UPDATE_TIME_STEP = 20;
 constexpr auto MAX_FPS = 144;
