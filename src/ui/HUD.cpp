f583bab4ec9578605cc9062205f9572fe2151c4b
NamPE286 <nambuihung654@gmail.com>
2025-03-12 22:47:10 +0700

    health bar

--- src/ui/HUD.cpp
+++ src/ui/HUD.cpp
@@ -14,15 +14,22 @@ HUD::HUD(SDL_Renderer* renderer, Player* player, SDL_Color color):
 
 	moneyText = new Text(renderer, Font::load("assets/fonts/stratum2-bold.ttf", 30), color);
 	moneyText->set_position(15, WINDOW_HEIGHT - 50);
+
+	hpText = new Text(renderer, Font::load("assets/fonts/stratum2-bold.ttf", 30), color);
+	hpText->set_position(WINDOW_WIDTH / 2 - 268, WINDOW_HEIGHT - 51);
 }
 
 void HUD::update() {
 	ammoText->set_content(std::to_string(player->get_weapon()->ammo));
 	reserveAmmoText->set_content(std::to_string(player->get_weapon()->reserveAmmo));
 	moneyText->set_content("$" + std::to_string(player->money));
+	hpText->set_content(std::to_string(player->hp));
+
+	healthBarRect.w = 40 * player->hp / 100;
 }
 
 void HUD::render() {
+
 	SDL_Rect ammoSep = {ammoText->rect.x + 58, ammoText->rect.y + 12, 2, 21};
 
 	if (player->get_weapon()->ammo != -1) {
@@ -33,5 +40,9 @@ void HUD::render() {
 		SDL_RenderFillRect(renderer, &ammoSep);
 	}
 
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+	SDL_RenderFillRect(renderer, &healthBarRect);
+
 	moneyText->render();
+	hpText->render();
 }
--- src/ui/HUD.hpp
+++ src/ui/HUD.hpp
@@ -5,11 +5,13 @@
 #include "../ui/Text.hpp"
 #include "../gameObjects/Player.hpp"
 #include "../behaviours/MonoBehaviour.hpp"
+#include "../common.h"
 
 class HUD : public MonoBehaviour {
 	Player* player = nullptr;
-	Text *ammoText = nullptr, *reserveAmmoText = nullptr, *moneyText = nullptr;
+	Text *ammoText = nullptr, *reserveAmmoText = nullptr, *moneyText = nullptr, *hpText = nullptr;
 	SDL_Color color = { 0, 0, 0, 0 };
+	SDL_Rect healthBarRect = { WINDOW_WIDTH / 2 - 267, WINDOW_HEIGHT - 20, 40, 4 };
 
 public:
 	HUD(SDL_Renderer* renderer, Player* player, SDL_Color color);
